c624c3a393f3846b82b3e24ffc54e198e79b5ba3
Ryan Turnbull <ryanturnbullemail@gmail.com>
2017-12-18 18:09:22 +1300

    Add about section, restyled transition, and add grad image

--- animations/menuFunctions.js
+++ animations/menuFunctions.js
@@ -1,5 +1,17 @@
+var screen = 0;
+
 function animateBurger(x) {
     x.classList.toggle("burgerChange");
+    if(screen == 1){
+      console.log($('#aboutScreen').css("left"));
+      if($('#aboutScreen').css("left") == (150+"px")){
+        $('#aboutScreen').css("left", "0px");
+        $('#aboutScreen').css("right", "300px");
+      }else{
+        $('#aboutScreen').css("left", "150px");
+        $('#aboutScreen').css("right", "150px");
+      }
+    }
 }
 
 function showAboutScreen(){
@@ -7,4 +19,5 @@ function showAboutScreen(){
   $('#welcomeScreen').fadeOut('slow');
   $('#aboutScreen').delay(1000);
   $('#aboutScreen').fadeIn(250);
+  screen = 1;
 }
